0d407c8dc80b5fb34faa0d8969dec332f351523c
vkvikas07-code <vkvikas07@gmail.com>
2026-09-08 12:35:15 +0530

    Load Admin Studio button style fixes

--- src/main.tsx
+++ src/main.tsx
@@ -1,21 +1,41 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+
 import { setupIonicReact } from '@ionic/react';
+
 import '@ionic/react/css/core.css';
 import '@ionic/react/css/normalize.css';
 import '@ionic/react/css/structure.css';
 import '@ionic/react/css/typography.css';
 import '@ionic/react/css/padding.css';
 import '@ionic/react/css/flex-utils.css';
+
 import './theme.css';
+import './admin-fixes.css';
+
 import App from './App';
 
 setupIonicReact();
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
-  <React.StrictMode><App /></React.StrictMode>
-);
+ReactDOM
+  .createRoot(
+    document.getElementById('root')!
+  )
+  .render(
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>
+  );
 
-if ('serviceWorker' in navigator && import.meta.env.PROD) {
-  window.addEventListener('load', () => navigator.serviceWorker.register('./sw.js'));
+if (
+  'serviceWorker' in navigator &&
+  import.meta.env.PROD
+) {
+  window.addEventListener(
+    'load',
+    () =>
+      navigator.serviceWorker.register(
+        './sw.js'
+      )
+  );
 }
